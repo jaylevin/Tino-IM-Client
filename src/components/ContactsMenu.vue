f8507fb6a154dbedf91b12638a9b6ff0408ef15d
jaylevin <jlevin12@saddleback.edu>
2018-10-23 17:10:21 -0700

    More responsive messages listview/message input box, improved contacts menu sidebar to be full-height

--- src/components/ContactsMenu.vue
+++ src/components/ContactsMenu.vue
@@ -1,12 +1,15 @@
 <template>
   <aside class="menu contacts">
     <p class="menu-label">
-      Conacts
+      Contacts
     </p>
     <ul class="menu-list">
-      <li v-for="contact in contacts" @click="selectedContact = contact"><a :class="{'is-active': selectedContact == contact}">{{contact}}</a></li>
+      <li v-for="contact in contacts" @click="selectedContact = contact">
+        <a :class="{'is-active': selectedContact == contact}">
+        {{contact}}
+        </a>
+      </li>
     </ul>
-
   </aside>
 </template>
 
@@ -31,7 +34,34 @@ export default {
 </script>
 
 <style scoped lang="scss">
+$grey-darker:  hsl(0, 0%, 21%) !default
+$grey-dark:    hsl(0, 0%, 29%) !default
+$accent: #00a1ff;
+
+.menu {
+  p {
+    color: $accent;
+    font-size:1em;
+  }
+  ul {
+    li {
+      .is-active {
+        background: $accent;
+      }
+      a {
+        color: white;
+        &:hover {
+          color: black;
+          background: $accent;
+        }
+      }
+    }
+  }
+}
 .contacts {
+  background: $grey-darker;
+  padding: 15px;
+  height: 80vh;
   text-align: left;
 }
 </style>
--- src/components/MessagesListView.vue
+++ src/components/MessagesListView.vue
@@ -7,14 +7,33 @@
 
         </message>
     </ul>
+
+    <!-- Pagination controls -->
+    <nav class="pagination is-rounded is-hidden-mobile" role="navigation" aria-label="pagination">
+      <a class="pagination-previous">Previous</a>
+      <a class="pagination-next">Next page</a>
+      <ul class="pagination-list">
+        <li><a class="pagination-link" aria-label="Goto page 1">1</a></li>
+        <li><span class="pagination-ellipsis">&hellip;</span></li>
+        <li><a class="pagination-link" aria-label="Goto page 45">45</a></li>
+        <li><a class="pagination-link" aria-label="Page 46" aria-current="page">46</a></li>
+        <li><a class="pagination-link" aria-label="Goto page 47">47</a></li>
+        <li><span class="pagination-ellipsis">&hellip;</span></li>
+        <li><a class="pagination-link" aria-label="Goto page 86">86</a></li>
+      </ul>
+    </nav>
+
+    <!-- Send message controls -->
     <div style="margin-top:0.5em;" class="is-divider"></div>
-    <div class="field has-addons">
-      <textarea class="textarea" placeholder="e.g. Hello world"></textarea>
-    </div>
-    <div class="control">
-      <a @click="sendMessage(messageInput)" class="button is-info">
-        Send
-      </a>
+    <div class="sendMessageDiv">
+      <div class="field has-addons">
+        <textarea class="textarea" placeholder="Type something..."></textarea>
+      </div>
+      <div class="control">
+        <a @click="sendMessage(messageInput)" class="button is-info">
+          Send
+        </a>
+      </div>
     </div>
   </div>
 </template>
@@ -82,14 +101,23 @@ export default {
 
 
 <style scoped lang="scss">
+.sendMessageDiv {
+  margin-top: 15px;
+  position: fixed;
+  bottom: 5px;
+  width: 80%;
+}
 .messages-ul {
   height: 70vh;
   overflow-y: scroll;
 }
+
 .messages {
+  border: 1px solid grey;
+  padding: 5px;
   .title {
     text-align: left;
   }
-  margin-right: 50px;
+  margin-right: 15px;
 }
 </style>
