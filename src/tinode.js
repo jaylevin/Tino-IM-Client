5264acfd46e8e663ede1646f3c1aa6b1faf8c8af
jaylevin <jlevin12@saddleback.edu>
2019-01-10 10:41:08 -0800

    Switch from DigitalOceans remote server to localhost (Tinode)

--- src/tinode.js
+++ src/tinode.js
@@ -1,7 +1,7 @@
 var Tinode = require("tinode-sdk");
-const API_KEY = "AQEAAAABAAD_rAp4DJh05a1HAwFT3A6K";
+const API_KEY = "AQAAAAABAABgdIf4e-qYHGhzx0pBm4tV";
 const APP_NAME = "Tinode-IM";
-const HOST = "167.99.158.63:6060";
+const HOST = "localhost:6060";
 import * as tinode from "./tinode.js";
 import router from "./router.js";
 
@@ -9,7 +9,7 @@ export function NewClient() {
   let client = new Tinode(APP_NAME, HOST, API_KEY, null, false);
   client.contacts = {};
 
-  // client.enableLogging(true);
+  client.enableLogging(true);
   client.onDisconnect = function() {
     console.log("Client has disconnected!");
     router.push({ name: "landing" });
