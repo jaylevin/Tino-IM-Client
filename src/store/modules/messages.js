77aa665d2f571260361428374043f1de6879e0a3
jaylevin <jlevin12@saddleback.edu>
2019-03-04 15:14:35 -0800

    Potential fix for #10: reset state on logout

--- src/store/modules/messages.js
+++ src/store/modules/messages.js
@@ -8,7 +8,7 @@ function defaultState() {
 }
 
 export default {
-  state: defaultState,
+  state: defaultState(),
 
   mutations: {
     storeMessage(state, message) {
@@ -43,6 +43,9 @@ export default {
       selectedTopic.publishMessage(msg).then(ctrl => {
         console.log("publish msg ctrl:", ctrl);
       });
+    },
+    clearMessagesState(state) {
+      state = Object.assign(state, defaultState());
     }
   }, // END of mutations
 
